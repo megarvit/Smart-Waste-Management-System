ce80ecf3efdaab091c057a35508ac9c7bfc297d7
Siddhant Patel <96189956+siddhantt10@users.noreply.github.com>
2023-12-10 17:17:23 +0530

    test routes
    test routes

--- smart-waste-management-system/src/App.js
+++ smart-waste-management-system/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/home/Home";
+import Navbar from "./Components/navbar/Navbar";
+import Footer from "./Components/footer/Footer";
+
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
-      {/* three file header home and footer */}
+      <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Home />
+                <Footer />
+              </>
+            }
+          />
+      </Routes>
     </div>
   );
 }
